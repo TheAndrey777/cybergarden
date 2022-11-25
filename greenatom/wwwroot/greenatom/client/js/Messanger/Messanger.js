03bbc2cca3ae4c54b40e75696d557e6358396dcc
RomaTheKing <103886943+RomaTheKing@users.noreply.github.com>
2022-11-26 01:55:27 +0300

    Login js

---
+++ greenatom/wwwroot/greenatom/client/js/Messanger/Messanger.js
@@ -0,0 +1,28 @@
+class Messenger {
+    constructor() {
+
+    }
+
+    send(request) {
+        axios.get('/'.concat(request.address, "/", request.message))
+            .then((response) => {
+                if (request.receive) request.receive(response);
+            })
+            .catch((error) => {
+                if (request.cache) request.cache(error);
+            });
+    }
+
+    connect(login, password) {
+        this.send({
+            address: "login",
+            message: login.concat(",", password),
+            receive: () => console.log("Connection open"),
+            cache: () => console.log("Failed connection open")
+        });
+    }
+
+    close() {
+
+    }
+}
