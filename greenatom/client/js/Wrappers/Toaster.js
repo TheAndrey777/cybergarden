de4b9b6b034cdda39ae464866e9b7d4c810da3f2
RomaTheKing <103886943+RomaTheKing@users.noreply.github.com>
2022-11-25 23:23:13 +0300

    add toast multi color support

--- greenatom/client/js/Wrappers/Toaster.js
+++ greenatom/client/js/Wrappers/Toaster.js
@@ -1,4 +1,11 @@
 const DEFAULT_TIME = 8000;
+const DEFAULT_COLOR_KEY = "green";
+const DEFAULT_COLOR = {
+    green: {start: "33B30A", finish: "7AC234"},
+    blue: {start: "4853A9", finish: "5A67CE"},
+    red: {start: "D7317A", finish: "B22865"}
+};
+
 
 class Toaster {
     constructor(toaster) {
@@ -6,6 +13,7 @@ class Toaster {
     }
 
     addToast(toast) {
+        let color = DEFAULT_COLOR[toast.color || DEFAULT_COLOR_KEY];
         Toastify({
             text: (toast.sender ? (toast.sender + "\n") : "") + toast.message || "",
             duration: toast.time || this.defaultTime,
@@ -16,7 +24,7 @@ class Toaster {
             position: "right",
             stopOnFocus: true,
             style: {
-                background: "linear-gradient(to right, #33B30A, #7AC234)",
+                background: "linear-gradient(to right, #".concat(color.start, ", #").concat(color.finish),
             },
             onClick: function () {
             }
