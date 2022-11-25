828b46746e40d7c02bc0c6b73189aaa711fad7cd
nullptr <roman.pytkov@mail.ru>
2022-11-26 02:34:42 +0300

    Merged

--- greenatom/Program.cs
+++ greenatom/Program.cs
@@ -1,40 +1,28 @@
+using greenatom.Models;
+using greenatom.Services;
 using Microsoft.AspNetCore.Authentication.Cookies;
 
-namespace greenatom;
+namespace greenatom ;
 
-public class Program
-{
-    public static void Main(string[] args)
+    public class Program
     {
-        var builder = WebApplication.CreateBuilder(args);
-
-        builder.Services.AddControllers();
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
-
-        builder.Services.Configure<DatabaseConfig>(builder.Configuration.GetSection("Database"));
-        builder.Services.AddSingleton<DatabaseService>();
-
-        var app = builder.Build();
-
-        if (app.Environment.IsDevelopment())
+        public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
             builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
                 .AddCookie(options => options.LoginPath = "/login");
             builder.Services.AddAuthorization();
 
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+            builder.Services.Configure<DatabaseConfig>(builder.Configuration.GetSection("Database"));
+            builder.Services.AddSingleton<DatabaseService>();
             builder.Services.AddControllers();
 
             var app = builder.Build();
 
-            //Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
@@ -43,31 +31,11 @@ public class Program
 
             app.UseHttpsRedirection();
 
-            //app.UseAuthentication();
             app.UseAuthorization();
-
             app.UseStaticFiles();
 
             app.MapControllers();
 
             app.Run();
         }
-
-        app.UseHttpsRedirection();
-
-        app.UseAuthorization();
-        app.UseStaticFiles();
-
-        app.UseFileServer(new FileServerOptions
-        {
-            FileProvider = new PhysicalFileProvider(
-                Path.Combine(builder.Environment.ContentRootPath, "client")),
-            //RequestPath = "/",
-            EnableDirectoryBrowsing = true
-        });
-
-        app.MapControllers();
-
-        app.Run();
     }
-}
