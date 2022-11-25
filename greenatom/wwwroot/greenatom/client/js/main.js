efac870ee183917361257e2f890583361d861f01
RomaTheKing <103886943+RomaTheKing@users.noreply.github.com>
2022-11-26 01:57:05 +0300

    Revert "Login js"
    This reverts commit 03bbc2cca3ae4c54b40e75696d557e6358396dcc.

--- greenatom/wwwroot/greenatom/client/js/main.js
+++
@@ -1,9 +0,0 @@
-let messenger = new Messenger();
-messenger.connect("tom@gmail.com", "12345");
-let count = 1;
-let toaster = new Toaster({});
-setInterval(() => {
-    toaster.addToast({message: "Какое-то уведомление", title: "Основная страница", color: "green"});
-    toaster.addToast({message: "Какое-то уведомление", title: "Основная страница", color: "blue"});
-    toaster.addToast({message: "Какое-то уведомление", title: "Основная страница", color: "red"});
-}, 1300);
