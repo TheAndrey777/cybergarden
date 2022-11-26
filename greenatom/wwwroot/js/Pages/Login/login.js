ffc30fd20b9e21cec564a5717fd0bca34b7609c2
RomaTheKing <103886943+RomaTheKing@users.noreply.github.com>
2022-11-26 09:53:14 +0300

    Add common validation

--- greenatom/wwwroot/js/Pages/Login/login.js
+++ greenatom/wwwroot/js/Pages/Login/login.js
@@ -3,8 +3,7 @@ let toaster = new Toaster();
 document.getElementById("connectButton").addEventListener("click", () => {
     let email = document.getElementById("email").value;
     let password = document.getElementById("password").value;
-    if (email.length === 0) toaster.addToast({message: "Введите email", title: "Ошибка:", color: "red"});
-    //else if (!(email.includes("@gmail.com") && email.includes("@mail.com"))) toaster.addToast({message: "Введите настоящий email", title: "Ошибка:", color: "red"});
-    else if (password.length === 0) toaster.addToast({message: "Введите пароль", title: "Успешно:", color: "red"});
-    else messenger.login(document.getElementById("email").value, document.getElementById("password").value);
+    let emailException = getEmailValidException(email), passwordException = getPasswordValidException(password);
+    if (emailException || passwordException) toaster.addToast({message: emailException || passwordException, title: "Ошибка:", color: "red"});
+    else messenger.login(email, password);
 });
--- greenatom/wwwroot/js/Pages/Register/register.js
+++ greenatom/wwwroot/js/Pages/Register/register.js
@@ -3,9 +3,7 @@ let toaster = new Toaster();
 document.getElementById("registerButton").addEventListener("click", () => {
     let email = document.getElementById("email").value;
     let password = document.getElementById("password").value, repeatPassword = document.getElementById("repeatPassword").value;
-    if (email.length === 0) toaster.addToast({message: "Введите email", title: "Ошибка:", color: "red"});
-    //else if (!(email.includes("@gmail.com") && email.includes("@mail.com"))) toaster.addToast({message: "Введите настоящий email", title: "Ошибка:", color: "red"});
-    else if (password.length === 0) toaster.addToast({message: "Введите пароль", title: "Ошибка:", color: "red"});
-    else if (password !== repeatPassword) toaster.addToast({message: "Пароли должны совпадать", title: "Ошибка:", color: "red"});
+    let emailException = getEmailValidException(email), passwordException = getPasswordValidException(password, repeatPassword || password + "1");
+    if (emailException || passwordException) toaster.addToast({message: emailException || passwordException, title: "Ошибка:", color: "red"});
     else messenger.register(email, password);
 });
