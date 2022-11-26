8fa35d6a2a16c8180fddad7f71eabdba1cbf508d
RomaTheKing <103886943+RomaTheKing@users.noreply.github.com>
2022-11-26 16:58:32 +0300

    correct UI

--- greenatom/wwwroot/js/Messanger/Messanger.js
+++ greenatom/wwwroot/js/Messanger/Messanger.js
@@ -19,7 +19,7 @@ class Messenger {
             address: "login",
             message: {email: email, password: password},
             receive: (response) => {
-                if (response.data.URL !== window.location.href) window.location = response.data.URL;
+                if (!window.location.href.includes(response.data.URL)) window.location = response.data.URL;
                 else this.toaster.addToast({message: "Не верные данные", title: "Ошибка:", color: "red"});
             },
             cache: (error) => {
--- greenatom/wwwroot/js/Pages/Login/login.js
+++ greenatom/wwwroot/js/Pages/Login/login.js
@@ -1,5 +1,20 @@
 let messenger = new Messenger();
 let toaster = new Toaster();
+
+function login(email, password) {
+    this.sendPost({
+        address: "login",
+        message: {email: email, password: password},
+        receive: (response) => {
+            if (response.data.URL !== window.location.href) window.location = response.data.URL;
+            else this.toaster.addToast({message: "Не верные данные", title: "Ошибка:", color: "red"});
+        },
+        cache: (error) => {
+            console.log(error)
+        }
+    });
+}
+
 document.getElementById("connectButton").addEventListener("click", () => {
     let email = document.getElementById("email").value;
     let password = document.getElementById("password").value;
@@ -7,3 +22,4 @@ document.getElementById("connectButton").addEventListener("click", () => {
     if (emailException || passwordException) toaster.addToast({message: emailException || passwordException, title: "Ошибка:", color: "red"});
     else messenger.login(email, password);
 });
+
